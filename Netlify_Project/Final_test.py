ba393296c4f6d98b528c8efe8be01c470e2262b9
Magda <you@example.com>
2024-04-20 22:55:19 +0100

    Adição de 2 novos testes

--- Netlify_Project/Final_test.py
+++ Netlify_Project/Final_test.py
@@ -4,6 +4,7 @@ from Pages_POM_Factory.initial_page import InitialPage
 from selenium.webdriver.common.by import By
 from Singleton.singleton import WebDriverSingleton
 from time import time,sleep
+import requests
 
 @pytest.fixture()
 def site_before_test():
@@ -19,7 +20,7 @@ def close_browser():
     WebDriverSingleton.quit_instance()
 
 # Colocar um user e testar se abre o github na zona inferior
-def test_git_hub(site_before_test):
+def atest_git_hub(site_before_test):
     driver = site_before_test
 
     name = 'magdarocha'
@@ -32,3 +33,36 @@ def test_git_hub(site_before_test):
     follow = driver.find_element(By.XPATH, "//a[contains(@href,'github.com')]")
     link_to_github = follow.get_attribute('href')
     assert link_to_github == 'https://github.com/' + name.lower().strip().replace(' ','' )
+
+# Abrir um user git hub, obter o link do user e verificar 
+#se é um site válido ou não, usar a biblioteca requests
+
+def atest_git_hub_002(site_before_test):
+    driver = site_before_test
+
+    name = 'magdarocha'
+
+    user = InitialPage(driver)
+    user.find_github_username(name)
+
+    link = user.get_link()
+    request_response = requests.head(link)
+    status_code = request_response.status_code
+    assert status_code == 200
+
+# Verificar se o número de followers a que se refere em cima corresponde ao número da lista em baixo
+
+def test_git_hub_003(site_before_test):
+    driver = site_before_test
+
+    name = 'magdarocha'
+
+    user = InitialPage(driver)
+    user.find_github_username(name)
+
+    sleep(1)
+
+    followers = user.get_followers()
+    follows_list = str(user.get_followers_num_list())
+
+    assert followers == follows_list
--- Netlify_Project/Pages_POM_Factory/initial_page.py
+++ Netlify_Project/Pages_POM_Factory/initial_page.py
@@ -1,10 +1,31 @@
 from Pages_POM_Factory.base_page import BasePage
 from seleniumpagefactory.Pagefactory import PageFactory
+from selenium.webdriver.common.by import By
 
 class InitialPage(BasePage, PageFactory):
     locators = {'username':('XPATH','//body /div /main /section /div /form /div /input'), 
-                'search':('XPATH','//body /div /main /section /div /form /div /button')}
+                'search':('XPATH','//body /div /main /section /div /form /div /button'),
+                'link':('XPATH','//body/div/main/section[3]/div/article/div/a'),
+                'followers':('XPATH', "//p[text()='followers']/preceding-sibling::h3"),
+                'followers_list':('XPATH', "//div[@class='followers']/article")}
+    
+    FOLLOWERS = (By.XPATH, "//div[@class='followers']/article")
    
     def find_github_username(self, username):
         self.username.set_text(username)
         self.search.click_button()
+
+    def get_link(self):
+        link = self.link.getAttribute('href')
+        return link
+    
+    def get_followers(self):
+        followers_num = self.followers.get_text()
+        return followers_num
+    
+    def get_followers_num_list(self):
+        followers_nums = self.driver.find_elements(*self.FOLLOWERS)
+        n = len(followers_nums)
+        return n
+    
+        
