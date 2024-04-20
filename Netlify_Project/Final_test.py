501d87f53e665b97a35ef50a43da6abb596c6dcc
Magda <you@example.com>
2024-04-20 23:11:14 +0100

    coloquei prints e dois testes a correr

--- Netlify_Project/Final_test.py
+++ Netlify_Project/Final_test.py
@@ -20,7 +20,7 @@ def close_browser():
     WebDriverSingleton.quit_instance()
 
 # Colocar um user e testar se abre o github na zona inferior
-def atest_git_hub(site_before_test):
+def test_git_hub(site_before_test):
     driver = site_before_test
 
     name = 'magdarocha'
@@ -33,6 +33,7 @@ def atest_git_hub(site_before_test):
     follow = driver.find_element(By.XPATH, "//a[contains(@href,'github.com')]")
     link_to_github = follow.get_attribute('href')
     assert link_to_github == 'https://github.com/' + name.lower().strip().replace(' ','' )
+    print(' O teste passou. O github user corresponde ao nome')
 
 # Abrir um user git hub, obter o link do user e verificar 
 #se é um site válido ou não, usar a biblioteca requests
@@ -48,7 +49,8 @@ def atest_git_hub_002(site_before_test):
     link = user.get_link()
     request_response = requests.head(link)
     status_code = request_response.status_code
-    assert status_code == 200
+    assert status_code != 200
+    print('O teste passou, verifica-se que o site não abre')
 
 # Verificar se o número de followers a que se refere em cima corresponde ao número da lista em baixo
 
@@ -66,3 +68,4 @@ def test_git_hub_003(site_before_test):
     follows_list = str(user.get_followers_num_list())
 
     assert followers == follows_list
+    print('O teste passou, a lista de followers tem o mesmo número de followers')
